07f80348a90504f5b4d9114aff8d705d8e6926f7
Lasse Letager Hansen <lasse@letager.dk>
2016-11-09 09:26:23 +0100

    Initial commit

--- test/hotciv/standard/TestAlphaCiv.java
+++ test/hotciv/standard/TestAlphaCiv.java
@@ -13,23 +13,23 @@ import java.util.*;
 
     Updated Oct 2015 for using Hamcrest matchers
 
-   This source code is from the book 
+   This source code is from the book
      "Flexible, Reliable Software:
        Using Patterns and Agile Development"
      published 2010 by CRC Press.
-   Author: 
-     Henrik B Christensen 
+   Author:
+     Henrik B Christensen
      Department of Computer Science
      Aarhus University
-   
+
    Please visit http://www.baerbak.com/ for further information.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
- 
+
        http://www.apache.org/licenses/LICENSE-2.0
- 
+
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -50,6 +50,30 @@ public class TestAlphaCiv {
   @Test
   public void shouldBeRedAsStartingPlayer() {
     assertThat(game.getPlayerInTurn(), is(Player.RED));
+  }
+
+  // There is a red city at (1,1)
+  @Test
+  public void redCityAt1comma1(){
+
+  }
+
+  // There is ocean at (1,0)
+  @Test
+  public void oceanAt1comma0(){
+
+  }
+
+  // Units cannot move over mountains
+  @Test
+  public void unitCannotMoveOverMountain(){
+
+  }
+
+  // Red cannot move blue units
+  @Test
+  public void redCannotMoveBlue(){
+
   }
 
 }
