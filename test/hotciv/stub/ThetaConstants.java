8d29f30aff5972c12269f0e5ceffe1e6c4c10a4e
Lasse Letager Hansen <lasse@letager.dk>
2016-12-15 10:40:15 +0100

    minidraw setup

---
+++ test/hotciv/stub/ThetaConstants.java
@@ -0,0 +1,12 @@
+package test.hotciv.stub;
+
+/** Covering constants used in various years
+ * of dSoftArk ThetaCiv implementations.
+ *
+ * Henrik Baerbak Christensen
+ */
+
+public class ThetaConstants {
+  public final static String CHARIOT = "chariot";
+  public final static String BOMB = "bomb";
+}
